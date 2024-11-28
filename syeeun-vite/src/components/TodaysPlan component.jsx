cad6f5f36d829a41371b2f6cf54e9b57622a8073
syeeun09 <sienna7669@gmail.com>
2024-11-28 17:37:39 +0900

    리액트

---
+++ syeeun-vite/src/components/TodaysPlan component.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class TodaysPlan extends Component {
+    render() {
+        return (
+            <div className="message-container">
+                유럽여행가고싶다아아아
+                나도 갈래 크리스마스 마켓~~~~~😭
+            </div>
+        );
+    }
+ }
+
+ export default TodaysPlan
